60c0f7f9a2baa2bb4c88e1444d15242fd69f2072
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-11-11 08:52:00 -0300

    Aula 22 Modulos e Pacotes

---
+++ uteis/numeros/__init__.py
@@ -0,0 +1,15 @@
+# Aula 22 - Módulos e pacotes
+
+def fatorial(n):
+    f = 1
+    for c in range(1, n + 1):
+        f *= c
+    return f
+
+
+def dobro(n):
+    return n*2
+
+
+def triplo(n):
+    return n*3
