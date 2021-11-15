d25fdec0ead6938577817e06442d40b24e059429
JonathanPMoraes <88660067+jonathanlbt1@users.noreply.github.com>
2021-11-14 21:13:27 -0300

    Exercicio 111

---
+++ main_program.py
@@ -0,0 +1,6 @@
+from Exercicio_111.utilidadescev import moedas, dado
+
+preco = float(input("Digite o preço: R$"))
+a = int(input("Digite o aumento: "))
+r = int(input("Digite a redução: "))
+dado.formatacao(preco, a, r)
